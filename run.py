91ab547e023db726ef514389037e288bd898fce6
leonardo fragoso <leonardorfragoso@gmail.com>
2024-12-21 10:11:57 -0300

    Ajustando filtros para os indicadores principais

--- run.py
+++ run.py
@@ -214,19 +214,38 @@ if missing_columns:
     st.stop()
 
 # Filters
-with st.expander("Filtros", expanded=True):  # A seção começa aberta
+with st.expander("Filtros", expanded=False):  # A seção começa fechada
+    # Filtros de data
     data_inicio = st.date_input("Data de Início", value=datetime(datetime.now().year, 1, 1))
     data_fim = st.date_input("Data Final", value=datetime(datetime.now().year, 12, 31))
+
+    # Filtros para Código de Infração e Descrição de Infração
     codigo_infracao_opcoes = data[8].dropna().unique()
     descricao_infracao_opcoes = data[11].dropna().unique()
-    codigo_infracao_selecionado = st.multiselect("Selecione o Código da Infração", options=codigo_infracao_opcoes, default=codigo_infracao_opcoes.tolist())
-    descricao_infracao_selecionada = st.multiselect("Selecione a Descrição da Infração", options=descricao_infracao_opcoes, default=descricao_infracao_opcoes.tolist())
-    valor_min, valor_max = st.slider("Selecione o intervalo de valores das multas", min_value=float(data[14].min()), max_value=float(data[14].max()), value=(float(data[14].min()), float(data[14].max())), step=0.01)
-    placa_opcoes = data[1].dropna().unique()
-    placa_selecionada = st.multiselect("Selecione a Placa do Veículo", options=placa_opcoes, default=placa_opcoes.tolist())
+    codigo_infracao_selecionado = st.multiselect(
+        "Selecione o Código da Infração", options=codigo_infracao_opcoes, default=codigo_infracao_opcoes.tolist())
+    descricao_infracao_selecionada = st.multiselect(
+        "Selecione a Descrição da Infração", options=descricao_infracao_opcoes, default=descricao_infracao_opcoes.tolist())
+    
+    # Filtro de valores das multas
+    valor_min, valor_max = st.slider(
+        "Selecione o intervalo de valores das multas",
+        min_value=float(data[14].min()),
+        max_value=float(data[14].max()),
+        value=(float(data[14].min()), float(data[14].max())),
+        step=0.01
+    )
 
-# Apply filters
-filtered_data = data[(data[9] >= pd.Timestamp(data_inicio)) & (data[9] <= pd.Timestamp(data_fim))]
+    # Filtro de placas de veículos
+    placa_opcoes = data[1].dropna().unique()
+    placa_selecionada = st.multiselect(
+        "Selecione a Placa do Veículo", options=placa_opcoes, default=placa_opcoes.tolist())
+
+# Aplicar filtros
+filtered_data = data[
+    (data[9] >= pd.Timestamp(data_inicio)) & 
+    (data[9] <= pd.Timestamp(data_fim))
+]
 filtered_data = filtered_data[filtered_data[8].isin(codigo_infracao_selecionado)]
 filtered_data = filtered_data[filtered_data[11].isin(descricao_infracao_selecionada)]
 filtered_data = filtered_data[(filtered_data[14] >= valor_min) & (filtered_data[14] <= valor_max)]
