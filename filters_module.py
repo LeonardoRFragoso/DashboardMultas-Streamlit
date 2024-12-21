f0b23100fad64d52cb3f887a1bdd820efcad6d9b
leonardo fragoso <leonardorfragoso@gmail.com>
2024-12-21 10:29:25 -0300

    Ajustando filtros para os indicadores principais

--- filters_module.py
+++ filters_module.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from datetime import datetime
 import pandas as pd
+from datetime import datetime
 
 def apply_filters(data):
     with st.expander("Filtros", expanded=True):
@@ -11,20 +11,20 @@ def apply_filters(data):
         descricao_infracao_opcoes = data[11].dropna().unique()
         
         codigo_infracao_selecionado = st.multiselect(
-            "Selecione o Código da Infração",
-            options=codigo_infracao_opcoes,
+            "Selecione o Código da Infração", 
+            options=codigo_infracao_opcoes, 
             default=codigo_infracao_opcoes.tolist()
         )
         
         descricao_infracao_selecionada = st.multiselect(
-            "Selecione a Descrição da Infração",
-            options=descricao_infracao_opcoes,
+            "Selecione a Descrição da Infração", 
+            options=descricao_infracao_opcoes, 
             default=descricao_infracao_opcoes.tolist()
         )
         
         valor_min, valor_max = st.slider(
             "Selecione o intervalo de valores das multas",
-            min_value=float(data[14].min()),
+            min_value=float(data[14].min()), 
             max_value=float(data[14].max()),
             value=(float(data[14].min()), float(data[14].max())),
             step=0.01
@@ -32,16 +32,16 @@ def apply_filters(data):
         
         placa_opcoes = data[1].dropna().unique()
         placa_selecionada = st.multiselect(
-            "Selecione a Placa do Veículo",
-            options=placa_opcoes,
+            "Selecione a Placa do Veículo", 
+            options=placa_opcoes, 
             default=placa_opcoes.tolist()
         )
 
-    # Aplicar os filtros ao dataframe
+    # Aplicar os filtros
     filtered_data = data[(data[9] >= pd.Timestamp(data_inicio)) & (data[9] <= pd.Timestamp(data_fim))]
     filtered_data = filtered_data[filtered_data[8].isin(codigo_infracao_selecionado)]
     filtered_data = filtered_data[filtered_data[11].isin(descricao_infracao_selecionada)]
     filtered_data = filtered_data[(filtered_data[14] >= valor_min) & (filtered_data[14] <= valor_max)]
     filtered_data = filtered_data[filtered_data[1].isin(placa_selecionada)]
-
+    
     return filtered_data
