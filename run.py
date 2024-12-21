63d82c633931c3c6c202def9cc1cd7851dcbf56f
leonardo fragoso <leonardorfragoso@gmail.com>
2024-12-21 10:19:12 -0300

    Ajustando filtros para os indicadores principais

--- run.py
+++ run.py
@@ -96,6 +96,9 @@ except KeyError as e:
     st.error(f"Erro ao carregar os segredos: {e}")
     st.stop()
 
+# Streamlit setup
+st.set_page_config(page_title="Multas Dashboard", layout="wide")
+
 # CSS Styling
 st.markdown(
     """
@@ -207,9 +210,6 @@ missing_columns = [col for col in required_columns if col not in data.columns]
 if missing_columns:
     st.error(f"As seguintes colunas estão ausentes: {missing_columns}")
     st.stop()
-
-# Streamlit setup
-st.set_page_config(page_title="Multas Dashboard", layout="wide")
 
 # CSS Styling
 st.markdown(
